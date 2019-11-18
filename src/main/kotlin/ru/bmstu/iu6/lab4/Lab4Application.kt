a5b65e51f99675097b5852eeb1c9edd81b7c21e9
erm0shin <danil.ermoshin7@mail.ru>
2019-11-18 21:35:45 +0300

    Init

---
+++ src/main/kotlin/ru/bmstu/iu6/lab4/Lab4Application.kt
@@ -0,0 +1,11 @@
+package ru.bmstu.iu6.lab4
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lab4Application
+
+fun main(args: Array<String>) {
+	runApplication<Lab4Application>(*args)
+}
